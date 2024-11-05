65563ced23d7ffcaafcc4d57d57591aff8f053e3
DiegoLinares11 <lin221256@uvg.edu.gt>
2024-11-05 08:10:02 -0600

    Conexion con firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -68,7 +69,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    //  implementation 'com.google.android.gms:play-services-maps:18.1.0' // SDK DE GOOGLEMAPS
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation(libs.firebase.analytics)
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+    // implementation 'com.google.android.gms:play-services-maps:18.1.0' // SDK DE GOOGLEMAPS
     //    implementation 'com.google.maps.android:maps-compose:2.11.2'     // JETPACK PARA IMPLEMENTAR
 }
