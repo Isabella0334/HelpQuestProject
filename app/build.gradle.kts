84207b01b1b2d17bd95c77efea97c8de69038882
DiegoLinares11 <lin221256@uvg.edu.gt>
2024-11-21 08:04:44 -0600

    dependency .play.services.maps on builgradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
